7a0d4e7571219e488cfcbc816053b293c9a70813
Lluis Adan <lluis.adan@gmail.com>
2026-03-04 23:56:02 +0100

    chore: prepare monolith deploy on Fly

--- api/app.js
+++ api/app.js
@@ -2,7 +2,7 @@ require('dotenv').config();
 
 const express = require('express');
 const logger = require('morgan');
-const cors = require('./middlewares/cors.middleware');
+const cors = require('cors');
 
 require('./configs/db.config');
 
